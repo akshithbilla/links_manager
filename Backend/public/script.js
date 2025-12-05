fe13a5bef9c6cbb1b27d55a61150080bccf20c77
akshithbilla <billaakshith@gmail.com>
2025-12-05 12:32:11 +0530

    ui

--- Backend/public/script.js
+++ Backend/public/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://links-manager-ph6d.onrender.com/api";
+const API_BASE = "http://localhost:5000/api";
 
 // DOM Elements
 const folderForm = document.getElementById("folder-form");
